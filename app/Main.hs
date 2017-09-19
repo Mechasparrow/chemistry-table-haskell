a79cc3f75b0e362b6b898b68dfb9bfe5fcea42f5
Michael Navazhylau <mikipux7@gmail.com>
2017-09-18 21:03:56 -0500

    created simple input loop

--- app/Main.hs
+++ app/Main.hs
@@ -5,11 +5,20 @@ import Reader
 
 main :: IO ()
 main = do
-  
+  loop
+
+
+loop :: IO ()
+loop = do
+
   putStrLn "Input an atomic number to find the element associated with it."
   raw_atomic_number <- getLine
-
 
   let atomic_number = read raw_atomic_number :: Int
   our_elem <- getElement(atomic_number)
   displayElement(return our_elem)
+  putStrLn ("")
+
+  if raw_atomic_number == "EXIT"
+    then return ()
+    else loop
